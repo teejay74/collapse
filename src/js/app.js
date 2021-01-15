5eda392d0e828983e7c67f82f792ef1f84bc43d0
teejay74 <teejay744@gmail.com>
2021-01-15 11:15:29 +0500

    task added

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+const btnCollapse = document.querySelector('.btn-collapse');
+const collapseElem = document.querySelector('.collapse');
+
+btnCollapse.onclick = () => {
+  btnCollapse.classList.toggle('opened');
+  btnCollapse.classList.contains('opened') ? collapseElem.classList.remove('show') : collapseElem.classList.add('show');
+};
